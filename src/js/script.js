0b94bffd1654059d8feb75e789be61c0e2e0e7cb
HeeSeongShin <64695494+HeeSeongShin@users.noreply.github.com>
2022-10-01 16:45:48 +0900

    Delete src directory

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-function iframeToUrl(url){
-    if(url != document.getElementById('frame').src){
-        document.getElementById('frame').src = url;
-    }else if(document.getElementById('frame').scrollTop != 0){
-        document.getElementById('frame').scrollTop = 0;
-    }
-}
